d64b95a69bd4b9b7959065b1e61224aa906e9bac
expandsource <expandsource@gmail.com>
2024-02-29 16:23:50 +0900

    익명 객체와 연습문제

--- src/nested/ex/ex3/Ex3.java
+++ src/nested/ex/ex3/Ex3.java
@@ -9,4 +9,13 @@ public class Ex3 {
         static class Engine {}
     }
      */
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        // 타이어 생성
+        Car.Tire tire = car.new Tire();
+
+        // 엔진 생성
+        Car.Engine engine = new Car.Engine();
+    }
 }
