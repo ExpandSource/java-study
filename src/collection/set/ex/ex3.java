d51f1dd3961e91acbf54a994ce889683e07d3bf8
expandsource <expandsource@gmail.com>
2024-03-13 12:50:25 +0900

    Set, HashSet

---
+++ src/collection/set/ex/ex3.java
@@ -0,0 +1,40 @@
+package collection.set.ex;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class ex3 {
+    /*
+    연습문제: 로또 번호 생성기
+
+    1부터 45까지의 숫자 중에서 6개의 숫자를 추출합니다.
+    HashSet을 활용하여 중복되지 않는 로또 번호 6개를 무작위로 생성하는 간단한 번호 생성기를 구현합니다.
+
+    요구 사항
+    - 1부터 45까지의 숫자 중에서 무작위로 6개의 숫자를 선택합니다.
+    - 선택된 숫자는 중복되면 안 됩니다.
+    - 6개의 숫자를 모두 선택한 후, 선택된 숫자들을 출력합니다.
+
+    출력 예시
+    ===
+    로또 번호: [5, 12, 23, 25, 33, 42]
+
+    Hint : Random 객체 사용
+     */
+    public static void main(String[] args) {
+        // 랜덤 객체 생성
+        Random random = new Random();
+        // 로또 번호를 담을 Set -> 중복 번호 없음
+        Set<Integer> lottoNumberSet = new HashSet<>();
+
+        // 항상 6개가 되면 종료
+        while (lottoNumberSet.size() < 6) {
+            int lottoNumber = random.nextInt(45) + 1;
+            lottoNumberSet.add(lottoNumber);
+        }
+
+        // 출력
+        System.out.println(lottoNumberSet);
+    }
+}
---
+++ src/collection/set/ex/ex5.java
@@ -0,0 +1,35 @@
+package collection.set.ex;
+
+public class ex5 {
+    /*
+    연습문제: 고유 캐릭터 목록 관리 시스템
+    목적
+    온라인 게임에서 플레이어가 생성할 수 있는 캐릭터들을 관리하는 시스템을 구현해봅니다. 각 캐릭터는 고유한 ID와 이름을 가지며, 시스템은 중복된 캐릭터 생성을 방지해야 합니다. 이 연습문제를 통해 객체의 고유성을 활용하여 중복 없는 데이터를 관리하는 방법을 실습합니다.
+
+    요구 사항
+    Character 클래스 정의
+
+    필드: 캐릭터 ID(String id), 캐릭터 이름(String name)
+    생성자: 캐릭터 ID와 이름을 매개변수로 받아 초기화
+    hashCode()와 equals() 메서드: 캐릭터 ID를 기준으로 오버라이드
+    Main 함수에서 캐릭터 목록 관리
+
+    여러 Character 객체를 생성하고, HashSet에 추가합니다.
+    캐릭터 ID가 같은 경우, HashSet에 중복 추가되지 않도록 합니다.
+    HashSet에 저장된 모든 캐릭터 정보(ID와 이름)를 출력합니다.
+
+
+    예시 입력 및 출력
+
+    입력: 캐릭터 정보(캐릭터 ID와 이름)
+    ===
+    캐릭터1: 캐릭터 ID "char001", 이름 "Dragon Warrior"
+    캐릭터2: 캐릭터 ID "char002", 이름 "Elf Archer"
+    캐릭터3: 캐릭터 ID "char001", 이름 "Mystic Mage" (캐릭터 ID가 캐릭터1과 동일)
+
+    출력: HashSet에 저장된 캐릭터 정보 출력
+    ===
+    "캐릭터 정보: ID char001, 이름 Dragon Warrior"
+    "캐릭터 정보: ID char002, 이름 Elf Archer"
+     */
+}
