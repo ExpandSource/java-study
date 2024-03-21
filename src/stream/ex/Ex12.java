abdc1f608b6e847b72c5e547092f4937c0f3f6b9
expandsource <expandsource@gmail.com>
2024-03-21 12:17:30 +0900

    스트림API 종합 연습문제

--- src/stream/ex/Ex12.java
+++ src/stream/ex/Ex12.java
@@ -1,5 +1,8 @@
 package stream.ex;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Ex12 {
     public static void main(String[] args) {
         /*
@@ -25,5 +28,30 @@ public class Ex12 {
     Developer 평균 급여: 5500.0
     Manager 평균 급여: 7500.0
          */
+        record Employee(String name, String role, int salary) {}
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("Alice", "Developer", 5000),
+                new Employee("Bob", "Developer", 6000),
+                new Employee("Charlie", "Manager", 7000),
+                new Employee("David", "Manager", 8000)
+        );
+
+        // 목표 1: 모든 직원의 총 급여 계산하기
+        Integer totalSalary = employees.stream()
+                .collect(Collectors.summingInt(Employee::salary));
+
+        System.out.println("총 급여 : " + totalSalary);
+
+        // 목표 2: 직책별 평균 급여 계산하기
+        Map<String, Double> byRoleSalaryAverage = employees.stream()
+                .collect(Collectors.groupingBy(Employee::role,
+                        Collectors.averagingDouble(Employee::salary)
+                ));
+
+        byRoleSalaryAverage.forEach(
+                (role, salaryAvg) ->
+                        System.out.printf("%s 평균 급여 : %.1f\n", role, salaryAvg)
+        );
     }
 }
